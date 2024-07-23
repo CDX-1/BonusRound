07f84ca522d74a1a9419c1bb2b327bc00b7dfec7
CDX <80155611+CDX-1@users.noreply.github.com>
2024-07-23 15:09:46 -0400

    Deleted old src

--- src/main/kotlin/net/cdx/bonusround/extensions/PlayerExtensions.kt
+++
@@ -1,7 +0,0 @@
-package net.cdx.bonusround.extensions
-
-import net.cdx.bonusround.data.PlayerDataProvider
-import org.bukkit.entity.Player
-
-val Player.dataProvider
-    get() = PlayerDataProvider.getPlayer(this)
--- src/main/kotlin/net/cdx/bonusround/games/api/GameEvent.kt
+++
@@ -1,3 +0,0 @@
-package net.cdx.bonusround.games.api
-
-data class GameEvent(val eventId: String, val parameters: HashMap<String, Any> = hashMapOf())
